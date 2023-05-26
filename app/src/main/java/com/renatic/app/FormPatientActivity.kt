e3bee312567ca97fa97d20cc1257003301109f73
lckmnzans <lckmnzans.elektro@gmail.com>
2023-05-26 18:17:27 +0700

    init project: UI half-complete

---
+++ app/src/main/java/com/renatic/app/FormPatientActivity.kt
@@ -0,0 +1,12 @@
+package com.renatic.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class FormPatientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_patient)
+    }
+}
---
+++ app/src/main/java/com/renatic/app/ProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.renatic.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar2)
+        setSupportActionBar(toolbar)
+    }
+}
